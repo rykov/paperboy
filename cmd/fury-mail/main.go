d64082c47df83d45bb2a795172e1b6b07f0745e0
Michael Rykov <mrykov@gmail.com>
2017-02-06 19:39:18 +0800

    Initial commit

---
+++ cmd/fury-mail/main.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/rykov/paperboy/parser"
+	"github.com/ghodss/yaml"
+	"github.com/go-gomail/gomail"
+	"github.com/toorop/go-dkim"
+	"io"
+	"io/ioutil"
+	"net/url"
+	"os"
+	"path/filepath"
+	"text/template"
+)
+
+// Context for email template
+type tmplContext struct {
+	User     map[string]interface{}
+	Campaign map[string]interface{}
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		printUsageError(fmt.Errorf("Invalid arguments"))
+		return
+	}
+
+	// Load up template with frontmatter
+	email, err := parseTemplate(os.Args[1])
+	if err != nil {
+		printUsageError(err)
+		return
+	}
+
+	// Read and cast frontmatter
+	var fMeta map[string]interface{}
+	if meta, err := email.Metadata(); err == nil && meta != nil {
+		fMeta, _ = meta.(map[string]interface{})
+	}
+
+	// Parse email template for processing
+	tmpl, err := template.New("email").Parse(string(email.Content()))
+	if err != nil {
+		printUsageError(err)
+		return
+	}
+
+	// Load up recipient metadata
+	who, err := parseRecipients(os.Args[2])
+	if err != nil {
+		printUsageError(err)
+		return
+	}
+
+	// Dial up the sender
+	sender, err := dialSMTPURL(os.Getenv("SMTP_URL"))
+	if err != nil {
+		printUsageError(err)
+		return
+	}
+
+	// Load private key
+	keyBytes, _ := ioutil.ReadFile("dkim.private")
+
+	// Configure with DKIM signing
+	defer sender.Close()
+	dOpts := dkim.NewSigOptions()
+	dOpts.PrivateKey = keyBytes
+	dOpts.Domain = "gemfury.com"
+	dOpts.Selector = "rails"
+	dOpts.SignatureExpireIn = 3600
+	dOpts.AddSignatureTimestamp = true
+	dOpts.Canonicalization = "relaxed/relaxed"
+	dOpts.Headers = []string{
+		"Mime-Version", "To", "From", "Subject", "Reply-To",
+		"Sender", "Content-Transfer-Encoding", "Content-Type",
+	}
+
+	// DKIM-signing sender
+	sender = &dkimSendCloser{Options: dOpts, sc: sender}
+
+	// Send emails
+	m := gomail.NewMessage()
+	for _, w := range who {
+		var body bytes.Buffer
+		ctx := &tmplContext{User: w, Campaign: fMeta}
+		if err := tmpl.Execute(&body, ctx); err != nil {
+			printUsageError(err)
+			return
+		}
+
+		toEmail := w["email"].(string)
+		toName, _ := w["username"].(string)
+		m.SetAddressHeader("To", toEmail, toName)
+		m.SetHeader("From", fMeta["from"].(string))
+		m.SetHeader("Subject", fMeta["subject"].(string))
+		m.SetBody("text/plain", body.String())
+
+		fmt.Println("Sending email to ", m.GetHeader("To"))
+		if err := gomail.Send(sender, m); err != nil {
+			fmt.Println("  Could not send email: ", err)
+		}
+	}
+}
+
+func dialSMTPURL(smtpURL string) (gomail.SendCloser, error) {
+	// Dial to SMTP server (with SSL)
+	surl, err := url.Parse(smtpURL)
+	if err != nil {
+		return nil, err
+	}
+
+	// Authentication
+	user, pass := os.Getenv("SMTP_USER"), os.Getenv("SMTP_PASS")
+	if auth := surl.User; auth != nil {
+		pass, _ = auth.Password()
+		user = auth.Username()
+	}
+
+	// TODO: Split & parse port from url.Host
+	host, port := surl.Host, 465
+
+	// Dial SMTP server
+	d := gomail.NewDialer(host, port, user, pass)
+	d.SSL = true // Force SSL (TODO: use schema)
+	return d.Dial()
+}
+
+func parseRecipients(path string) ([]map[string]interface{}, error) {
+	fmt.Println("Loading recipients: ", path)
+	raw, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var out []map[string]interface{}
+	return out, yaml.Unmarshal(raw, &out)
+}
+
+func parseTemplate(path string) (parser.Email, error) {
+	fmt.Println("Loading template: ", path)
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+	return parser.ReadFrom(file)
+}
+
+func printUsageError(err error) {
+	base := filepath.Base(os.Args[0])
+	fmt.Printf("USAGE: %s [template] [recipients]\n", base)
+	fmt.Println("Error: ", err)
+}
+
+// ======= DKIM ========
+type dkimSendCloser struct {
+	Options dkim.SigOptions
+	sc      gomail.SendCloser
+}
+
+func (d *dkimSendCloser) Send(from string, to []string, msg io.WriterTo) error {
+	return d.sc.Send(from, to, dkimMessage{d.Options, msg})
+}
+
+func (d *dkimSendCloser) Close() error {
+	return d.sc.Close()
+}
+
+type dkimMessage struct {
+	options dkim.SigOptions
+	msg     io.WriterTo
+}
+
+func (dm dkimMessage) WriteTo(w io.Writer) (n int64, err error) {
+	var b bytes.Buffer
+	if _, err := dm.msg.WriteTo(&b); err != nil {
+		return 0, err
+	}
+
+	email := b.Bytes()
+	if err := dkim.Sign(&email, dm.options); err != nil {
+		return 0, err
+	}
+
+	return bytes.NewBuffer(email).WriteTo(w)
+}
---
+++ parser/frontmatter.go
@@ -0,0 +1,191 @@
+// Copyright 2015 The Hugo Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package parser
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	toml "github.com/pelletier/go-toml"
+
+	"gopkg.in/yaml.v2"
+)
+
+type frontmatterType struct {
+	markstart, markend []byte
+	Parse              func([]byte) (interface{}, error)
+	includeMark        bool
+}
+
+func InterfaceToConfig(in interface{}, mark rune) ([]byte, error) {
+	if in == nil {
+		return []byte{}, fmt.Errorf("input was nil")
+	}
+
+	b := new(bytes.Buffer)
+
+	switch mark {
+	case rune(YAMLLead[0]):
+		by, err := yaml.Marshal(in)
+		if err != nil {
+			return nil, err
+		}
+		b.Write(by)
+		_, err = b.Write([]byte("..."))
+		if err != nil {
+			return nil, err
+		}
+		return b.Bytes(), nil
+	case rune(TOMLLead[0]):
+		tree := toml.TreeFromMap(in.(map[string]interface{}))
+		return []byte(tree.String()), nil
+	case rune(JSONLead[0]):
+		by, err := json.MarshalIndent(in, "", "   ")
+		if err != nil {
+			return nil, err
+		}
+		b.Write(by)
+		_, err = b.Write([]byte("\n"))
+		if err != nil {
+			return nil, err
+		}
+		return b.Bytes(), nil
+	default:
+		return nil, fmt.Errorf("Unsupported Format provided")
+	}
+}
+
+func InterfaceToFrontMatter(in interface{}, mark rune) ([]byte, error) {
+	if in == nil {
+		return []byte{}, fmt.Errorf("input was nil")
+	}
+
+	b := new(bytes.Buffer)
+
+	switch mark {
+	case rune(YAMLLead[0]):
+		_, err := b.Write([]byte(YAMLDelimUnix))
+		if err != nil {
+			return nil, err
+		}
+		by, err := yaml.Marshal(in)
+		if err != nil {
+			return nil, err
+		}
+		b.Write(by)
+		_, err = b.Write([]byte(YAMLDelimUnix))
+		if err != nil {
+			return nil, err
+		}
+		return b.Bytes(), nil
+	case rune(TOMLLead[0]):
+		_, err := b.Write([]byte(TOMLDelimUnix))
+		if err != nil {
+			return nil, err
+		}
+
+		tree := toml.TreeFromMap(in.(map[string]interface{}))
+		b.Write([]byte(tree.String()))
+		_, err = b.Write([]byte("\n" + TOMLDelimUnix))
+		if err != nil {
+			return nil, err
+		}
+		return b.Bytes(), nil
+	case rune(JSONLead[0]):
+		by, err := json.MarshalIndent(in, "", "   ")
+		if err != nil {
+			return nil, err
+		}
+		b.Write(by)
+		_, err = b.Write([]byte("\n"))
+		if err != nil {
+			return nil, err
+		}
+		return b.Bytes(), nil
+	default:
+		return nil, fmt.Errorf("Unsupported Format provided")
+	}
+}
+
+func FormatToLeadRune(kind string) rune {
+	switch FormatSanitize(kind) {
+	case "yaml":
+		return rune([]byte(YAMLLead)[0])
+	case "json":
+		return rune([]byte(JSONLead)[0])
+	default:
+		return rune([]byte(TOMLLead)[0])
+	}
+}
+
+// TODO(bep) move to helpers
+func FormatSanitize(kind string) string {
+	switch strings.ToLower(kind) {
+	case "yaml", "yml":
+		return "yaml"
+	case "toml", "tml":
+		return "toml"
+	case "json", "js":
+		return "json"
+	default:
+		return "toml"
+	}
+}
+
+// DetectFrontMatter detects the type of frontmatter analysing its first character.
+func DetectFrontMatter(mark rune) (f *frontmatterType) {
+	switch mark {
+	case '-':
+		return &frontmatterType{[]byte(YAMLDelim), []byte(YAMLDelim), HandleYAMLMetaData, false}
+	case '+':
+		return &frontmatterType{[]byte(TOMLDelim), []byte(TOMLDelim), HandleTOMLMetaData, false}
+	case '{':
+		return &frontmatterType{[]byte{'{'}, []byte{'}'}, HandleJSONMetaData, true}
+	default:
+		return nil
+	}
+}
+
+func HandleTOMLMetaData(datum []byte) (interface{}, error) {
+	m := map[string]interface{}{}
+	datum = removeTOMLIdentifier(datum)
+
+	tree, err := toml.Load(string(datum))
+
+	if err != nil {
+		return m, err
+	}
+
+	m = tree.ToMap()
+
+	return m, nil
+}
+
+func removeTOMLIdentifier(datum []byte) []byte {
+	return bytes.Replace(datum, []byte(TOMLDelim), []byte(""), -1)
+}
+
+func HandleYAMLMetaData(datum []byte) (interface{}, error) {
+	m := map[string]interface{}{}
+	err := yaml.Unmarshal(datum, &m)
+	return m, err
+}
+
+func HandleJSONMetaData(datum []byte) (interface{}, error) {
+	var f interface{}
+	err := json.Unmarshal(datum, &f)
+	return f, err
+}
