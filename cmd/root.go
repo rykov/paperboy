db07df553f6709dcfbdf9286da48166d38a34f70
Michael Rykov <mrykov@gmail.com>
2017-03-30 10:52:40 +0800

    Adopt Cobra for CLI functionality

--- cmd/root.go
+++ cmd/root.go
@@ -1,13 +1,8 @@
-package main
+package cmd
 
 import (
 	"bytes"
 	"fmt"
-	"github.com/rykov/paperboy/parser"
-	"github.com/ghodss/yaml"
-	"github.com/go-gomail/gomail"
-	"github.com/spf13/viper"
-	"github.com/toorop/go-dkim"
 	"io"
 	"io/ioutil"
 	"net/url"
@@ -15,7 +10,71 @@ import (
 	"path/filepath"
 	"text/template"
 	"time"
+
+	"github.com/rykov/paperboy/parser"
+	"github.com/ghodss/yaml"
+	"github.com/go-gomail/gomail"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/toorop/go-dkim"
 )
+
+var cfgFile string
+
+// RootCmd represents the base command when called without any subcommands
+var RootCmd = &cobra.Command{
+	Use:   "fury-mail",
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) {
+		sendCampaign(args)
+	},
+}
+
+// Execute adds all child commands to the root command sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports Persistent Flags, which, if defined here,
+	// will be global for your application.
+
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.fury-mail.yaml)")
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" { // enable ability to specify config file via flag
+		viper.SetConfigFile(cfgFile)
+	}
+
+	viper.SetConfigName(".fury-mail") // name of config file (without extension)
+	viper.AddConfigPath("$HOME")      // adding home directory as first search path
+	viper.AutomaticEnv()              // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
 
 // Global viper config
 var Config *viper.Viper
@@ -26,8 +85,8 @@ type tmplContext struct {
 	Campaign map[string]interface{}
 }
 
-func main() {
-	if len(os.Args) != 3 {
+func sendCampaign(args []string) {
+	if len(args) != 2 {
 		printUsageError(fmt.Errorf("Invalid arguments"))
 		return
 	}
@@ -49,7 +108,7 @@ func main() {
 	//}
 
 	// Load up template with frontmatter
-	email, err := parseTemplate(os.Args[1])
+	email, err := parseTemplate(args[0])
 	if err != nil {
 		printUsageError(err)
 		return
@@ -69,7 +128,7 @@ func main() {
 	}
 
 	// Load up recipient metadata
-	who, err := parseRecipients(os.Args[2])
+	who, err := parseRecipients(args[1])
 	if err != nil {
 		printUsageError(err)
 		return
