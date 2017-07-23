9c4a4f84dbfe97514936951a7eb09ef71a94494b
Michael Rykov <mrykov@gmail.com>
2017-07-23 16:23:09 +0800

    Remove legacy preview server cruft

--- cmd/server.go
+++ cmd/server.go
@@ -1,16 +1,11 @@
 package cmd
 
 import (
-	"github.com/rykov/paperboy/mail"
 	"github.com/rykov/paperboy/server"
 	"github.com/spf13/cobra"
 
-	"bytes"
 	"fmt"
-	"html"
-	"io"
 	"net/http"
-	"strings"
 )
 
 var serverCmd = &cobra.Command{
@@ -22,42 +17,8 @@ var serverCmd = &cobra.Command{
 			return err
 		}
 
-		fmt.Println("Preview server listening at :8080 ... ")
-		http.HandleFunc("/", serverPreview)
 		server.AddGraphQLRoutes()
-
+		fmt.Println("API server listening at :8080 ... ")
 		return http.ListenAndServe(":8080", nil)
 	},
-}
-
-func serverPreview(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<html><body>")
-	defer fmt.Fprintf(w, "</body></html>")
-
-	parts := strings.Split(r.URL.Path[1:], "/")
-	if len(parts) != 2 {
-		fmt.Fprintf(w, "Invalid URL")
-		return
-	}
-
-	template, who := parts[0], parts[1]
-	fmt.Fprintf(w, "<p>TEMPLATE: %s, RECIPIENTS: %s</p>", template, who)
-
-	c, err := mail.LoadCampaign(template, who)
-	if err != nil || len(c.Recipients) == 0 {
-		fmt.Fprintf(w, "Invalid campaign: %s", err)
-		return
-	}
-
-	fmt.Fprintf(w, "<pre>")
-	msg, err := c.MessageFor(0)
-	if err != nil {
-		fmt.Fprintf(w, "Campaign error: %s", err)
-		return
-	}
-
-	var buf bytes.Buffer
-	msg.WriteTo(&buf)
-	io.WriteString(w, html.EscapeString(buf.String()))
-	fmt.Fprintf(w, "</pre>")
 }
