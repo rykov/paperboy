732b2076a367c8ec9461ce5dc45bcf1ce3beae5e
Michael Rykov <mrykov@gmail.com>
2025-09-20 01:52:44 +0800

    Removed LoadConfig from "version" command

--- cmd/version.go
+++ cmd/version.go
@@ -12,8 +12,7 @@ func versionCmd() *cobra.Command {
 		Use:   "version",
 		Short: "Print the version number of Paperboy",
 		Run: func(cmd *cobra.Command, args []string) {
-			cfg, _ := config.LoadConfig(cmd.Context())
-			fmt.Printf("Paperboy Email Engine %s\n", cfg.Build)
+			fmt.Printf("Paperboy Email Engine %s\n", config.Build)
 		},
 	}
 }
